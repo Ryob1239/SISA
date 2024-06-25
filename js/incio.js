984f2067cec19a8ec04d49894d2b497cda219589
An1419Z <133408475+An1419Z@users.noreply.github.com>
2024-06-25 13:05:50 -0600

    Add files via upload

---
+++ js/incio.js
@@ -0,0 +1,22 @@
+    function handleLogin(event) {
+        event.preventDefault(); // Previene la acción por defecto del formulario
+
+        var usuario = document.getElementById('usuario').value;
+        var contrasena = document.getElementById('contrasena').value;
+
+        // Detectar la letra en el campo de usuario
+        var primeraLetra = usuario.charAt(0).toLowerCase();
+
+        var destino;
+        if (primeraLetra === 'a') {
+            destino = 'Alumno.html';
+        } else if (primeraLetra === 'd') {
+            destino = 'Docente.html';
+        } else {
+            alert('Usuario no válido');
+            return;
+        }
+
+        // Redirigir al usuario al destino correspondiente
+        window.location.href = destino;
+    }
---
+++ js/java.js
@@ -0,0 +1,8 @@
+document.querySelector('.logout').addEventListener('click', function(event) {
+    event.preventDefault(); // Previene la acción por defecto del enlace
+    var mensaje = 'Quiere cerrar sesión?'; // Cambia esto por el mensaje que desees
+    var confirmacion = confirm(mensaje);
+    if (confirmacion) {
+        window.location.href = 'Inicio.html'; // Asegúrate de que esta es la página a la que quieres redirigir
+    }
+});
