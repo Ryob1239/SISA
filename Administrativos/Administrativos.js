64f569e933c07a0794c3e2d331c0f43e541e6438
An1419Z <angelzatarain25@gmail.com>
2024-07-06 14:59:24 -0600

    Avance Administrativos

--- Administrativos/Administrativos.js
+++ Administrativos/Administrativos.js
@@ -6,3 +6,18 @@ document.querySelector('.logout').addEventListener('click', function(event) {
         window.location.href = '../Inicio/Inicio/Inicio.html'; // Asegúrate de que esta es la página a la que quieres redirigir
     }
 });
+
+document.querySelector('.SALONES').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevents the default action of the button
+    window.location.href = 'Agendacion/Agendacion.html'; // Redirects to the agendacion.html page
+});
+
+document.querySelector('.HORARIOS').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevents the default action of the button
+    window.location.href = 'AdministrarHo/Administrarho.html'; // Redirects to the agendacion.html page
+});
+
+document.querySelector('.TUTORADOS').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevents the default action of the button
+    window.location.href = 'AdministrarTu/AdministrarTu.html'; // Redirects to the agendacion.html page
+});
--- README.md
+++ README.md
@@ -8,6 +8,10 @@ LISTA DE PENDIENTES
     1.6-Registro de cuenta nueva (LISTO)
 2-Limpiar codigos
 3-Hacer el de administrativos
+    3.1-Hacer inicio (Listo)
+    3.2-Agendacion de salon     
+    3.3-Administracion Horarios
+    3.4-Administrar tutorados
 4-agendar salon y cambiar salon sera igual al de administrativos pero solo podran ver (Docentes)
 5-Diponibilidad de salones, horario de grupo igual al de administrativos pero que solo lo puedan ver (Docentes)
 6-Disponibilidad de salones, horario de grupo igual al docentes (Alumnos)
