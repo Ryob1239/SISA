51595f982473ce07969bb4a8bbe2d558e5962c1b
An1419Z <angelzatarain25@gmail.com>
2024-07-07 18:05:45 -0600

    AdministrarHo Teminado

--- Administrativos/AdministrarHo/Administrarho.js
+++ Administrativos/AdministrarHo/Administrarho.js
@@ -3,6 +3,19 @@ document.querySelector('.logout').addEventListener('click', function(event) {
     var mensaje = 'Quiere cerrar sesión?'; // Cambia esto por el mensaje que desees
     var confirmacion = confirm(mensaje);
     if (confirmacion) {
-        window.location.href = '../Inicio/Inicio/Inicio.html'; // Asegúrate de que esta es la página a la que quieres redirigir
+        window.location.href = '../../Inicio/Inicio/Inicio.html'; // Asegúrate de que esta es la página a la que quieres redirigir
     }
+});
+
+document.getElementById('TDM32').addEventListener('click', function() {
+    document.getElementById('tableh').style.display = 'block';
+    document.getElementById('buttons').style.display = 'block';
+    document.getElementById('Logo').style.display = 'none';
+});
+
+document.getElementById('back').addEventListener('click', function() {
+    
+    document.getElementById('tableh').style.display = 'none';
+    document.getElementById('buttons').style.display = 'none';
+    document.getElementById('Logo').style.display = 'block';
 });
